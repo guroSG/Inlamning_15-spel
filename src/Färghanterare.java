b71b94b9cf85d4b48552040187a62d0a82a215f8
guroSG <gustafhermans@gmail.com>
2022-11-18 09:01:08 +0100

    färgåterställare tillagd

---
+++ src/Färghanterare.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Färghanterare {
+    public static void ändraFärg(JButton knapp){
+        knapp.setBackground(Color.green);
+    }
+}
---
+++ src/Färgåterställare.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+public class Färgåterställare {
+    public static void återställFärg(JButton knapp){
+        knapp.setBackground(new JButton().getBackground());
+    }
+}
